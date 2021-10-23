fab3c3e6ec04f753e43da6d56a8e099dbfe08e58
HoshinoSora-728 <sora7pivo@icloud.com>
2021-10-23 10:36:58 +0900

    githubpages参考

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 // import Welcome from './components/Welcome';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/stickey-footer.css';
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/stickey-footer.css";
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
